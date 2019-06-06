7ae7445389da274265a4758ce424c24aab98a3b4
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-06-06 09:59:52 -0500

    First CoffeeStoreCommit
    Beverage set up

---
+++ CoffeeStore/Beverage.java
@@ -0,0 +1,35 @@
+public class Beverage{
+
+  private int size;
+  private int type;
+  private int base;
+  private float cost;
+  private float price;
+  private String productKey;
+  private int syrup;
+  private int topping;
+
+  public int getSize(){ return size;}
+  public void setSize(int s){ size = s;}
+
+  public int getType(){ return type;}
+  public void setType(int s){ type = s;}
+
+  public int getBase(){ return base;}
+  public void setBase(int s){ base = s;}
+
+  public float getCost(){ return cost;}
+  public void setCost(float s){ cost = s;}
+
+  public float getPrice(){ return price;}
+  public void setPrice(float s){ price = s;}
+
+  public String getProductKey(){ return productKey;}
+  public void setProductKey(String s){ productKey = s;}
+
+  public int getSyrup(){ return syrup;}
+  public void setSyrup(int s){ syrup = s;}
+
+  public int getTopping(){ return topping;}
+  public void setTopping(int s){ topping = s;}
+}
---
+++ CoffeeStore/MyCoffeeStore.java
@@ -0,0 +1,9 @@
+public class MyCoffeeStore{
+
+  
+  public static void main(String args[]){
+
+
+  }
+
+}
