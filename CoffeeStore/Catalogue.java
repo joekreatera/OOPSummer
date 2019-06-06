4a482bd6e199b30791796e5350bb3e0b26cefe4d
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-06-06 10:20:50 -0500

    Update to add beverages
    Just added beverages

--- CoffeeStore/Catalogue.java
+++ CoffeeStore/Catalogue.java
@@ -4,16 +4,11 @@ public class Catalogue{
 
   public Catalogue(){
       baseBeverages = new Beverage[5];
-      baseBeverages[0] = new Beverage(
-        Beverage.SMALL,
-        Beverage.HOT,
-        Beverage.COFFEE,
-        10.0f,
-        30.0f,
-        "Cafe basico",
-        Beverage.NONE,
-        Beverage.NONE
-      );
+      baseBeverages[0] = new Beverage(Beverage.SMALL,Beverage.HOT,Beverage.COFFEE,  10.0f,  30.0f,"Cafe basico",  Beverage.NONE,Beverage.NONE);
+      baseBeverages[1] = new Beverage(Beverage.LARGE,Beverage.HOT,Beverage.COFFEE,  15.0f,  45.0f,"Cafe grande",  Beverage.NONE,Beverage.NONE);
+      baseBeverages[2] = new Beverage(Beverage.SMALL,Beverage.COLD,Beverage.COFFEE,  10.0f,  30.0f,"Cafe de fresa ch",  Beverage.STRAWBERRY,Beverage.WHIPPED_CREAM);
+      baseBeverages[3] = new Beverage(Beverage.LARGE,Beverage.COLD,Beverage.COFFEE,  20.0f,  40.0f,"Cafe de fresa gd",  Beverage.STRAWBERRY,Beverage.WHIPPED_CREAM);
+      baseBeverages[4] = new Beverage(Beverage.LARGE,Beverage.COLD,Beverage.TEA,  5.0f,  40.0f,"Te basico",  Beverage.MANGO,Beverage.NONE);
 
   }
 
