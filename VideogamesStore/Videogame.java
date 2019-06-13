4f51a7ee34a3666bca7aa0d4a3abcedc733c6f77
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-06-13 09:45:25 -0500

    Updated Videogame Store
    Updated to load and save to File

--- VideogamesStore/Videogame.java
+++ VideogamesStore/Videogame.java
@@ -5,24 +5,34 @@ public class Videogame{
   String name;
   String rating;
   int stock;
+  private long id;
   public Videogame(String data){
       StringTokenizer tokens = new StringTokenizer(data,",");
       int dataNum = 0;
       while(tokens.hasMoreTokens()){
           String val = tokens.nextToken();
-          if( dataNum == 0) name = val;
-          if( dataNum == 1) rating = val;
-          if( dataNum == 2) price = Float.parseFloat(val);
-          if( dataNum == 3) stock = Integer.parseInt(val);
+          if( dataNum == 0) id = Long.parseLong(val);
+          if( dataNum == 1) name = val;
+          if( dataNum == 2) rating = val;
+          if( dataNum == 3) price = Float.parseFloat(val);
+          if( dataNum == 4) stock = Integer.parseInt(val);
           dataNum++;
       }
   }
+
+  public long getId(){ return id;}
   public float getPrice(){ return price;}
   public String getName(){ return name;}
   public String getRating(){return rating;}
   public int getStock(){ return stock;}
+  public void setId(long i){ id = i; }
   public void setPrice(float p){ price = p;}
   public void setName(String p){name = p;}
   public void setRating(String p){rating = p;}
   public void setStock(int p){ stock = p;}
+  @Override
+  public String toString(){
+      //Fifa 2000, M, 1200.00,4
+    return id+","+name + "," + rating + ","+ price + "," + stock;
+  }
 }
--- VideogamesStore/VideogameStore.java
+++ VideogamesStore/VideogameStore.java
@@ -5,6 +5,9 @@ public class VideogameStore{
 
   public VideogameStore(String dataFile){
     // initialize according to game database
+  }
+
+  public void loadDatabase(String dataFile){
     try{
       FileReader reader = new FileReader(dataFile);
       BufferedReader database= new  BufferedReader(reader);
@@ -30,8 +33,26 @@ public class VideogameStore{
       System.out.println("Exception " + ioe);
     }
   }
+
+  public void saveDatabase(String filename){
+      // please make backup before doing this!!!
+      try{
+          FileWriter file = new FileWriter(filename);
+          PrintWriter printer = new PrintWriter(file, true);
+
+          for(int i = 0; i < inventory.length ;i++){
+            printer.println( inventory[i] );
+          }
+          printer.close();
+
+      }catch(IOException ioe){
+        System.out.println("Exception " + ioe);
+      }
+  }
   public static void main(String args[]){
     VideogameStore vs = new VideogameStore("db.vg");
+    vs.loadDatabase("db.vg");
+    vs.saveDatabase("db.vg");
   }
 
 }
